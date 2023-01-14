d9e3e7b84726f6134e8b87682c8fb84d6c5097f2
EswaravakaAnitha <122293189+EswaravakaAnitha@users.noreply.github.com>
2023-01-14 19:53:17 +0530

    Add files via upload

---
+++ decrementation.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class decrementation {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b;
+		b=++a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
---
+++ incrementation.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class incrementation {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b;
+		b=a++;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
