8a8f0b557057e37dc54719dda82510e4d9813736
EswaravakaAnitha <122293189+EswaravakaAnitha@users.noreply.github.com>
2023-01-14 18:58:10 +0530

    Add files via upload

---
+++ Explicit_type_casting.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Explicit_type_casting {
+
+	public static void main(String[] args) {
+		double a=34.5;
+		byte b;
+		b=(byte) a;
+		System.out.println(b);
+	}
+
+}
---
+++ Implicit_type_casting.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Implicit_type_casting {
+	public static void main(String[] args) {
+	
+		byte a=9;//1 byte
+		double b;//8 bytes
+		b=a;
+		System.out.println(b);
+		
+	}
+
+}
