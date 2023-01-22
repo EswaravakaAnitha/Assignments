29f237e1eb4bec3c3e1cca44eb8434358a60129a
EswaravakaAnitha <122293189+EswaravakaAnitha@users.noreply.github.com>
2023-01-22 22:51:06 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,36 @@
+package package1;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*for(int a=0;a<5;++a)
+		{
+			System.out.println("*");
+			
+		}
+		int n=10;
+		int i=0;
+		while(i<n)
+		{
+			System.out.print("*");
+			i++;
+		}
+		int i=0;
+		do {
+			System.out.println("*");
+			i++;
+		}
+		while(i>5);*/
+		int n=5;
+		for(int a=0;a<n;a++)
+		{
+			for(int i=0;i<n;i++) 
+			{
+			System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+}
