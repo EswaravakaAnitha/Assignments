de2693b87860e3cf5e9e6f7c035f150b5ac8abe0
EswaravakaAnitha <122293189+EswaravakaAnitha@users.noreply.github.com>
2023-03-22 23:01:48 +0530

    Add files via upload

---
+++ Array_add.java
@@ -0,0 +1,20 @@
+package package1;
+
+public class Array_add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int []a= {34,54,65,76,76,9};
+		int sum=0;
+		
+		for(int i=0;i<a.length;i++)
+		{
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
